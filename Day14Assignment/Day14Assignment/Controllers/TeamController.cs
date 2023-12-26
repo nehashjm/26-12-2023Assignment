1985aa70ac38b7d2a759cfe7d451ef87f4758bec
Nehash <nehash.j.m027@gmail.com>
2023-12-26 15:37:46 +0530

    day14Assignment

---
+++ Day14Assignment/Day14Assignment/Controllers/TeamController.cs
@@ -0,0 +1,34 @@
+﻿using Day14Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day14Assignment.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        static List<Team> teamList = new List<Team>()
+        {
+            new Team(){Id=1,Name="India",NOWC="2"},
+            new Team(){Id=2,Name="Australia",NOWC="4"},
+            new Team(){Id=3,Name="Westindies",NOWC="2"},
+            new Team(){Id=4,Name="England",NOWC="1"}
+        };
+        public ActionResult Index()
+        {
+            return View(teamList);
+        }
+        public ActionResult Display()
+        {
+            return View(teamList);
+        }
+        public ActionResult Create() 
+        {
+            return View(teamList);
+        }
+       
+    }
+}
---
+++ Day14Assignment/Day14Assignment/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day14Assignment.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NOWC { get; set; }
+    }
+}
